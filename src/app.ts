f1b407395e8233dfeb8f069175607576b610485d
PavitarSharma <pavitarsharma144@gmail.com>
2024-04-13 16:28:53 +0530

    Redis

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import express, { Application } from "express";
-import * as dotenv from 'dotenv'
-import cors from "cors"
-import helmet from "helmet"
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
 const app: Application = express();
-app.use(helmet()); 
-app.use(cors()); 
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        status: 200,
-        message: "Application successfully loaded"
-    })
-})
+  res.status(200).json({
+    status: 200,
+    message: "Application successfully loaded",
+  });
+});
 
-export default app
+export default app;
