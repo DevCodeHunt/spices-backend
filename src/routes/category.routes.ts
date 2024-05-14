9939b1e29dfd31ca913781a591c855919421cbb3
PavitarSharma <pavitarsharma144@gmail.com>
2024-05-14 22:38:49 +0530

    Category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,13 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
-import { ErrorHandler, UploadedFile, uploadImage } from "../utils";
+import { deleteImage, ErrorHandler, UploadedFile, uploadImage } from "../utils";
 import { isAdmin, isAuthenticated, upload } from "../middlewares";
 import { adminService } from "../services";
+import { Category } from "../models";
 
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.send([])
+        const categories = await Category.find()
+        res.status(200).send(categories)
     } catch (error: any) {
         return next(new ErrorHandler(500, error.message))
     }
@@ -15,12 +17,21 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
 
 router.post("/upload-image", upload.single("image"), async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const image = await uploadImage(req.file as UploadedFile)
+        const { imageId } = req.body;
+        let image
+        if (imageId) {
+            await deleteImage(imageId)
+            image = await uploadImage(req.file as UploadedFile)
+        } else {
+            image = await uploadImage(req.file as UploadedFile)
+        }
         res.status(200).send(image)
     } catch (error: any) {
         return next(new ErrorHandler(500, error.message))
     }
 })
+
+
 
 router.use(isAuthenticated, isAdmin)
 router.post("/", async (req: Request, res: Response, next: NextFunction) => {
@@ -36,27 +47,43 @@ router.post("/", async (req: Request, res: Response, next: NextFunction) => {
     }
 })
 
-router.put("/:id", upload.single("image"), async (req: Request, res: Response, next: NextFunction) => {
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { name, image } = req.body
         if (!name) return next(new ErrorHandler(400, "Category name is required"));
         if (!image) return next(new ErrorHandler(400, "Category image is required"));
 
-        const category = await adminService.addCategory(req.body)
+        const category = await adminService.updateCategory({
+            name,
+            image,
+            id: req.params.id,
+        })
         res.status(200).send(category)
     } catch (error: any) {
         return next(new ErrorHandler(500, error.message))
     }
 })
 
-router.delete("/:id", upload.single("image"), async (req: Request, res: Response, next: NextFunction) => {
+router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { name, image } = req.body
-        if (!name) return next(new ErrorHandler(400, "Category name is required"));
-        if (!image) return next(new ErrorHandler(400, "Category image is required"));
+        const { imageId } = req.query
+        await deleteImage(imageId as string)
 
-        const category = await adminService.addCategory(req.body)
+        const category = await adminService.deleteCategory(req.params.id)
         res.status(200).send(category)
+    } catch (error: any) {
+        return next(new ErrorHandler(500, error.message))
+    }
+})
+
+router.post("/deleteAllCategory", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { categories} = req.body
+        await adminService.deleteAllCategory(categories)
+        await Promise.all(categories.map(async (category: any) => {
+            await deleteImage(category.image.id)
+        }))
+        res.status(200).send(categories)
     } catch (error: any) {
         return next(new ErrorHandler(500, error.message))
     }
--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,23 +1,39 @@
-
 import { Category, Product } from "../models";
-import { AddProduct, TImage } from "../types"
+import { AddProduct, TImage } from "../types";
 
 class AdminService {
-    async addCategory({ name, image }: { name: string, image: TImage }) {
-        return await Category.create({ name, image});
-    }
+  async addCategory({ name, image }: { name: string; image: TImage }) {
+    return await Category.create({ name, image });
+  }
 
-    async editCategory({ name, image }: { name: string, image: TImage }) {
-        return "hello"
-    }
+  async updateCategory({
+    name,
+    image,
+    id,
+  }: {
+    name: string;
+    image: TImage;
+    id: string;
+  }) {
+    return await Category.findByIdAndUpdate(
+      id,
+      { name: name, image: image },
+      { new: true }
+    );
+  }
 
-    async deleteCategory() {
-        return "hello"
-    }
+  async deleteCategory(id: string) {
 
-    async createProduct(body: AddProduct) {
-        return await Product.create(body)
-    }
+    return await Category.findByIdAndDelete(id);
+  }
+
+  async deleteAllCategory(categories: any[]) {
+    return await Category.deleteMany({ _id: { $in: categories } });
+  }
+
+  async createProduct(body: AddProduct) {
+    return await Product.create(body);
+  }
 }
 
-export const adminService = new AdminService()
+export const adminService = new AdminService();
